2ba4d272842c4636136789405ecbd5c084d330fb
Puker Who <nawwesele@gmail.com>
2024-09-06 12:01:26 +0300

    edit

--- single-slots.php
+++ single-slots.php
@@ -265,7 +265,7 @@ $currentId = get_the_ID();
                           <path stroke-linecap="round" stroke-linejoin="round" d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5" />
                         </svg>
                       </div>
-                      <a href="#"><?php _e("Грати", "treba-wp"); ?>!</a>
+                      <a href="https://danabol.com.ua/go/play/"><?php _e("Грати", "treba-wp"); ?>!</a>
                     </div>
                   </td>
                 </tr>
